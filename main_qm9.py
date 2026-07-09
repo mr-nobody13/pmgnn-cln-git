73347f33b3ad120caada7c138b2fa2f17e8f1ffb
mr-nobody13 <mr13nobody13@gmail.com>
2026-07-10 00:20:44 +0330

    Update main_qm9.py

--- main_qm9.py
+++ main_qm9.py
@@ -276,9 +276,9 @@ def main():
     parser.add_argument("--num_rbf", type=int, default=16)
 
     # حالت basis reduction: SBF=3x4 و RBF=8
-    parser.add_argument("--reduced_num_spherical", type=int, default=4)
+    parser.add_argument("--reduced_num_spherical", type=int, default=5)
     parser.add_argument("--reduced_num_radial", type=int, default=4)
-    parser.add_argument("--reduced_num_rbf", type=int, default=10)
+    parser.add_argument("--reduced_num_rbf", type=int, default=8)
 
     # -----------------------------
     # تنظیمات خروجی و لاگ
